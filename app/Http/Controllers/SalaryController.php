3b917dd4fc1c6bfdb1fc1d1803c764eb3091f0b3
ahmad <ahmed.mostafa@homzmart.com>
2021-12-22 13:16:18 +0200

    add

--- app/Http/Controllers/SalaryController.php
+++ app/Http/Controllers/SalaryController.php
@@ -9,6 +9,7 @@ class SalaryController extends Controller
 {
 
     protected $recommendtionService;
+    protected $key;
 
     /**
      * Create a new controller instance.
@@ -18,6 +19,8 @@ class SalaryController extends Controller
     public function __construct(RecommendtionService $recommendtionService)
     {
         $this->recommendtionService = $recommendtionService;
+        $this->key = 'u0TbYfzVhnAs6LvGyI7Oq4o4gZSPS22e70RkTCpo';
+
     }
 
     public function getCountries()
@@ -25,7 +28,7 @@ class SalaryController extends Controller
 
         $getData = (new \App\Helpers\GetService(['Accept' => 'application/hal+json',
             'Accept-Language' => 'en',
-            'X-Auth-Token' => 'XoKQXNuE7tnBVD9c3ZQz4rtrSHixznRylAf0EIRT',
+            'X-Auth-Token' => $this->key,
         ], 'https://paylab.com/paylab_api/v1/countries'));
         return response($getData->json(), $getData->status());
         //return response(;
@@ -36,7 +39,7 @@ class SalaryController extends Controller
 
         $getData = (new \App\Helpers\GetService(['Accept' => 'application/hal+json',
             'Accept-Language' => 'en',
-            'X-Auth-Token' => 'XoKQXNuE7tnBVD9c3ZQz4rtrSHixznRylAf0EIRT',
+            'X-Auth-Token' => $this->key,
         ], 'https://paylab.com/paylab_api/v1/category_positions'));
         return response($getData->json(), $getData->status());
         //return response(;
@@ -49,7 +52,7 @@ class SalaryController extends Controller
 
         $getData = (new \App\Helpers\GetService(['Accept' => 'application/hal+json',
             'Accept-Language' => 'en',
-            'X-Auth-Token' => 'XoKQXNuE7tnBVD9c3ZQz4rtrSHixznRylAf0EIRT',
+            'X-Auth-Token' => $this->key,
         ], "https://paylab.com/paylab_api/v1//country/$country/category_position/$catpos/advanced"));
         return response($getData->json(), $getData->status());
         //return response(;
